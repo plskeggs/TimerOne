9aeaba8f73b8e3a585b5d64b6853bbcf2cd65132
Pete Skeggs <plskeggs@noeticdesign.com>
2013-02-24 11:24:01 -0800

    - in sw pwm mode, allow user's ISR callback to handle I/O

--- TimerOne.cpp
+++ TimerOne.cpp
@@ -42,207 +42,220 @@
 
 #define FAST_PWM // PETE: use this when using non-timer pins
 
-TimerOne Timer1;              // preinstatiate
+TimerOne Timer1;                                                               // preinstatiate
 
-ISR(TIMER1_OVF_vect)          // interrupt service routine that wraps a user defined function supplied by attachInterrupt
+ISR(TIMER1_OVF_vect)                                                           // interrupt service routine that wraps a user defined function supplied by attachInterrupt
 {
-  if (Timer1.isrCallback)
-    Timer1.isrCallback();
-  if (Timer1.enableSwPwm)
-    digitalWrite(Timer1.outputPin, 1);
+   if (Timer1.isrCallback)
+      Timer1.isrCallback(true);
+   else if (Timer1.enableSwPwm)                                                // if we're in software pwm, let user handle PIO if they set a callback, otherwise we do it here
+      digitalWrite(Timer1.outputPin, 1);                                       // warning: this takes about 25us at 8MHz, so setPeriod() should be called with a sufficiently large value that you don't
+                                                                               // notice how coarse the least significant time interval is
 }
 
 ISR(TIMER1_COMPA_vect)
 {
-  if (Timer1.enableSwPwm)
-    digitalWrite(Timer1.outputPin, 0);
+   if (Timer1.enableSwPwm)
+   {
+      if (Timer1.isrCallback)                                                  // only for software pwm, do another callback on the falling edge
+         Timer1.isrCallback(false);
+      else
+         digitalWrite(Timer1.outputPin, 0);
+   }
 }
 
 void TimerOne::initialize(long microseconds)
 {
-  isrCallback = NULL;
-  enableSwPwm = false;
+   isrCallback = NULL;
+   enableSwPwm = false;
 #if defined(FAST_PWM)
-  TCCR1A = _BV(WGM11);                 // set mode 14: fast pwm, 10 bit, 
-  TCCR1B = _BV(WGM13) | _BV(WGM12);    // ICR1 is TOP
+   TCCR1A = _BV(WGM11);                                                        // set mode 14: fast pwm, 10 bit, 
+   TCCR1B = _BV(WGM13) | _BV(WGM12);                                           // ICR1 is TOP
 #else
-  TCCR1A = 0;                          // clear control register A 
-  TCCR1B = _BV(WGM13);                 // set mode 8: phase and frequency correct pwm, stop the timer
+   TCCR1A = 0;                                                                 // clear control register A 
+   TCCR1B = _BV(WGM13);                                                        // set mode 8: phase and frequency correct pwm, stop the timer
 #endif
-  setPeriod(microseconds);
+   setPeriod(microseconds);
 }
 
 
-void TimerOne::setPeriod(long microseconds)		// AR modified for atomic access
+void TimerOne::setPeriod(long microseconds)                                    // AR modified for atomic access
 {
-  
-  long cycles = (F_CPU / 2000000) * microseconds;                                // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
-  if(cycles < RESOLUTION)              clockSelectBits = _BV(CS10);              // no prescale, full xtal
-  else if((cycles >>= 3) < RESOLUTION) clockSelectBits = _BV(CS11);              // prescale by /8
-  else if((cycles >>= 3) < RESOLUTION) clockSelectBits = _BV(CS11) | _BV(CS10);  // prescale by /64
-  else if((cycles >>= 2) < RESOLUTION) clockSelectBits = _BV(CS12);              // prescale by /256
-  else if((cycles >>= 2) < RESOLUTION) clockSelectBits = _BV(CS12) | _BV(CS10);  // prescale by /1024
-  else        cycles = RESOLUTION - 1, clockSelectBits = _BV(CS12) | _BV(CS10);  // request was out of bounds, set as maximum
-  
-  oldSREG = SREG;				
-  cli();							// Disable interrupts for 16 bit register access
-  ICR1 = pwmPeriod = cycles;                                          // ICR1 is TOP in p & f correct pwm mode
-  SREG = oldSREG;
-  
-  TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
-  TCCR1B |= clockSelectBits;                                          // reset clock select register, and starts the clock
+
+   long cycles = (F_CPU / 2000000) * microseconds;                             // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
+   if (cycles < RESOLUTION)              clockSelectBits = _BV(CS10);          // no prescale, full xtal
+   else if ((cycles >>= 3) < RESOLUTION) clockSelectBits = _BV(CS11);          // prescale by /8
+   else if ((cycles >>= 3) < RESOLUTION) clockSelectBits = _BV(CS11) | _BV(CS10);   // prescale by /64
+   else if ((cycles >>= 2) < RESOLUTION) clockSelectBits = _BV(CS12);          // prescale by /256
+   else if ((cycles >>= 2) < RESOLUTION) clockSelectBits = _BV(CS12) | _BV(CS10);   // prescale by /1024
+   else        cycles = RESOLUTION - 1, clockSelectBits = _BV(CS12) | _BV(CS10);   // request was out of bounds, set as maximum
+
+   oldSREG = SREG;          
+   cli();                                                                      // Disable interrupts for 16 bit register access
+   ICR1 = pwmPeriod = cycles;                                                  // ICR1 is TOP in p & f correct pwm mode
+   SREG = oldSREG;
+
+   TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
+   TCCR1B |= clockSelectBits;                                                  // reset clock select register, and starts the clock
 }
 
 void TimerOne::setPwmDuty(char pin, int duty)
 {
-  unsigned long dutyCycle = pwmPeriod;
-  
-  dutyCycle *= duty;
-  dutyCycle >>= 10;
-  
-  oldSREG = SREG;
-  cli();
-  enableSwPwm = false;
-  if(pin == 1 || pin == 9)       OCR1A = dutyCycle;
-  else if(pin == 2 || pin == 10) OCR1B = dutyCycle;
-  else {
-   if (dutyCycle >= pwmPeriod)
-     dutyCycle = pwmPeriod - 1; // avoid setting OCR1A to TOP
-	OCR1A = dutyCycle;           // PLS: allow for any pin
-	outputPin = pin;
-	TIMSK1 = _BV(TOIE1) | _BV(OCIE1A); // at overflow, turn on pin; at output compare A, turn off
-	enableSwPwm = true;
-  }
-  SREG = oldSREG;
+   unsigned long dutyCycle = pwmPeriod;
+
+   dutyCycle *= duty;
+   dutyCycle >>= 10;
+
+   oldSREG = SREG;
+   cli();
+   enableSwPwm = false;
+   if (pin == 1 || pin == 9)       OCR1A = dutyCycle;
+   else if (pin == 2 || pin == 10) OCR1B = dutyCycle;
+   else
+   {
+      if (dutyCycle >= pwmPeriod)
+         dutyCycle = pwmPeriod - 1;                                            // avoid setting OCR1A to TOP
+      OCR1A = dutyCycle;                                                       // PLS: allow for any pin
+      outputPin = pin;
+      TIMSK1 = _BV(TOIE1) | _BV(OCIE1A);                                       // at overflow, turn on pin; at output compare A, turn off
+      enableSwPwm = true;
+   }
+   SREG = oldSREG;
 }
 
-void TimerOne::pwm(char pin, int duty, long microseconds)  // expects duty cycle to be 10 bit (1024)
+void TimerOne::pwm(char pin, int duty, long microseconds)                      // expects duty cycle to be 10 bit (1024)
 {
-  if(microseconds > 0) setPeriod(microseconds);
-  if(pin == 1 || pin == 9) {
-    DDRB |= _BV(PORTB1);                                   // sets data direction register for pwm output pin
-    TCCR1A |= _BV(COM1A1);                                 // activates the output pin
-  }
-  else if(pin == 2 || pin == 10) {
-    DDRB |= _BV(PORTB2);
-    TCCR1A |= _BV(COM1B1);
-  }
-  setPwmDuty(pin, duty);
-  resume();			// Lex - make sure the clock is running.  We don't want to restart the count, in case we are starting the second WGM
-					// and the first one is in the middle of a cycle
+   if (microseconds > 0) setPeriod(microseconds);
+   if (pin == 1 || pin == 9)
+   {
+      DDRB |= _BV(PORTB1);                                                     // sets data direction register for pwm output pin
+      TCCR1A |= _BV(COM1A1);                                                   // activates the output pin
+   }
+   else if (pin == 2 || pin == 10)
+   {
+      DDRB |= _BV(PORTB2);
+      TCCR1A |= _BV(COM1B1);
+   }
+   setPwmDuty(pin, duty);
+   resume();                                                                   // Lex - make sure the clock is running.  We don't want to restart the count, in case we are starting the second WGM
+   // and the first one is in the middle of a cycle
 }
 
 void TimerOne::disablePwm(char pin)
 {
-  if(pin == 1 || pin == 9)       TCCR1A &= ~_BV(COM1A1);   // clear the bit that enables pwm on PB1
-  else if(pin == 2 || pin == 10) TCCR1A &= ~_BV(COM1B1);   // clear the bit that enables pwm on PB2
-  else { // PLS: turn off interrupt for handling any other pin
-    oldSREG = SREG;
-    cli();
-    TIMSK1 &= ~_BV(OCIE1A);
-	if (!isrCallback)
-	  TIMSK1 &= ~_BV(TOIE1);
-	outputPin = pin;
-	digitalWrite(pin, 0); // turn off output
-    enableSwPwm = false;
-    SREG = oldSREG;
-  }
+   if (pin == 1 || pin == 9)       TCCR1A &= ~_BV(COM1A1);                     // clear the bit that enables pwm on PB1
+   else if (pin == 2 || pin == 10) TCCR1A &= ~_BV(COM1B1);                     // clear the bit that enables pwm on PB2
+   else                                                                        // PLS: turn off interrupt for handling any other pin
+   {
+      oldSREG = SREG;
+      cli();
+      TIMSK1 &= ~_BV(OCIE1A);
+      if (!isrCallback)
+         TIMSK1 &= ~_BV(TOIE1);
+      outputPin = pin;
+      digitalWrite(pin, 0);                                                    // turn off output
+      enableSwPwm = false;
+      SREG = oldSREG;
+   }
 }
 
-void TimerOne::attachInterrupt(void (*isr)(), long microseconds)
+void TimerOne::attachInterrupt(void (*isr)(bool overflow), long microseconds)
 {
-  if(microseconds > 0) setPeriod(microseconds);
-  isrCallback = isr;                                       // register the user's callback with the real ISR
-  TIMSK1 = _BV(TOIE1);                                     // sets the timer overflow interrupt enable bit
-	// AR - remove sei() - might be running with interrupts disabled (eg inside an ISR), so leave unchanged
+   if (microseconds > 0) setPeriod(microseconds);
+   isrCallback = isr;                                                          // register the user's callback with the real ISR
+   TIMSK1 = _BV(TOIE1);                                                        // sets the timer overflow interrupt enable bit
+   // AR - remove sei() - might be running with interrupts disabled (eg inside an ISR), so leave unchanged
 //  sei();                                                   // ensures that interrupts are globally enabled
-  resume();
+   resume();
 }
 
 void TimerOne::detachInterrupt()
 {
-  isrCallback = NULL; // PLS: ensure that if the user calls pwm() with a non-standard pin, we don't call the old isrCallback now
-  TIMSK1 &= ~_BV(TOIE1);                                   // clears the timer overflow interrupt enable bit 
+   isrCallback = NULL;                                                         // PLS: ensure that if the user calls pwm() with a non-standard pin, we don't call the old isrCallback now
+   TIMSK1 &= ~_BV(TOIE1);                                                      // clears the timer overflow interrupt enable bit 
 }
 
-void TimerOne::resume()				// AR suggested
-{ 
-  TCCR1B |= clockSelectBits;
-}
-
-void TimerOne::restart()		// Depricated - Public interface to start at zero - Lex 10/9/2011
+void TimerOne::resume()                                                        // AR suggested
 {
-	start();				
+   TCCR1B |= clockSelectBits;
 }
 
-void TimerOne::start()	// AR addition, renamed by Lex to reflect it's actual role
+void TimerOne::restart()                                                       // Depricated - Public interface to start at zero - Lex 10/9/2011
 {
-  unsigned int tcnt1;
-  
-  TIMSK1 &= ~_BV(TOIE1);        // AR added 
-  GTCCR |= _BV(PSRSYNC);   		// AR added - reset prescaler (NB: shared with all 16 bit timers);
+   start();          
+}
 
-  oldSREG = SREG;				// AR - save status register
-  cli();						// AR - Disable interrupts
-  TCNT1 = 0;                	
-  SREG = oldSREG;          		// AR - Restore status register
+void TimerOne::start()                                                         // AR addition, renamed by Lex to reflect it's actual role
+{
+   unsigned int tcnt1;
 
-  do {	// Nothing -- wait until timer moved on from zero - otherwise get a phantom interrupt
-	oldSREG = SREG;
-	cli();
-	tcnt1 = TCNT1;
-	SREG = oldSREG;
-  } while (tcnt1==0); 
- 
+   TIMSK1 &= ~_BV(TOIE1);                                                      // AR added 
+   GTCCR |= _BV(PSRSYNC);                                                      // AR added - reset prescaler (NB: shared with all 16 bit timers);
+
+   oldSREG = SREG;                                                             // AR - save status register
+   cli();                                                                      // AR - Disable interrupts
+   TCNT1 = 0;                  
+   SREG = oldSREG;                                                             // AR - Restore status register
+
+   do                                                                          // Nothing -- wait until timer moved on from zero - otherwise get a phantom interrupt
+   {
+      oldSREG = SREG;
+      cli();
+      tcnt1 = TCNT1;
+      SREG = oldSREG;
+   } while (tcnt1==0); 
+
 //  TIFR1 = 0xff;              		// AR - Clear interrupt flags
 //  TIMSK1 = _BV(TOIE1);              // sets the timer overflow interrupt enable bit
 }
 
 void TimerOne::stop()
 {
-  TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));          // clears all clock selects bits
+   TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));                             // clears all clock selects bits
 }
 
-unsigned long TimerOne::read()		//returns the value of the timer in microseconds
-{									//rember! phase and freq correct mode counts up to then down again
-  	unsigned long tmp;				// AR amended to hold more than 65536 (could be nearly double this)
-  	unsigned int tcnt1;				// AR added
+unsigned long TimerOne::read()                                                 //returns the value of the timer in microseconds
+{
+   //rember! phase and freq correct mode counts up to then down again
+   unsigned long tmp;                                                          // AR amended to hold more than 65536 (could be nearly double this)
+   unsigned int tcnt1;                                                         // AR added
 
-	oldSREG= SREG;
-  	cli();							
-  	tmp=TCNT1;    					
-	SREG = oldSREG;
+   oldSREG= SREG;
+   cli();                     
+   tmp=TCNT1;                 
+   SREG = oldSREG;
 
-	char scale=0;
-	switch (clockSelectBits)
-	{
-	case 1:// no prescalse
-		scale=0;
-		break;
-	case 2:// x8 prescale
-		scale=3;
-		break;
-	case 3:// x64
-		scale=6;
-		break;
-	case 4:// x256
-		scale=8;
-		break;
-	case 5:// x1024
-		scale=10;
-		break;
-	}
-	
-	do {	// Nothing -- max delay here is ~1023 cycles.  AR modified
-		oldSREG = SREG;
-		cli();
-		tcnt1 = TCNT1;
-		SREG = oldSREG;
-	} while (tcnt1==tmp); //if the timer has not ticked yet
+   char scale=0;
+   switch (clockSelectBits)
+   {
+      case 1:                                                                  // no prescalse
+         scale=0;
+         break;
+      case 2:                                                                  // x8 prescale
+         scale=3;
+         break;
+      case 3:                                                                  // x64
+         scale=6;
+         break;
+      case 4:                                                                  // x256
+         scale=8;
+         break;
+      case 5:                                                                  // x1024
+         scale=10;
+         break;
+   }
 
-	//if we are counting down add the top value to how far we have counted down
-	tmp = (  (tcnt1>tmp) ? (tmp) : (long)(ICR1-tcnt1)+(long)ICR1  );		// AR amended to add casts and reuse previous TCNT1
-	return ((tmp*1000L)/(F_CPU /1000L))<<scale;
+   do                                                                          // Nothing -- max delay here is ~1023 cycles.  AR modified
+   {
+      oldSREG = SREG;
+      cli();
+      tcnt1 = TCNT1;
+      SREG = oldSREG;
+   } while (tcnt1==tmp);                                                       //if the timer has not ticked yet
+
+   //if we are counting down add the top value to how far we have counted down
+   tmp = (  (tcnt1>tmp) ? (tmp) : (long)(ICR1-tcnt1)+(long)ICR1  );            // AR amended to add casts and reuse previous TCNT1
+   return((tmp*1000L)/(F_CPU /1000L))<<scale;
 }
 
-#endif
+#endif 
